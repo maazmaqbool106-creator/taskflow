4f588c0bb64274db35a77e920a53b235ba1fc0ae
Maaz Ur Rehman <maazmaqbool106@gmail.com>
2026-08-25 18:26:59 +0500

    Update API to deployed backend

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.18:5000/api",
+  baseURL: "https://taskflowbackend-rust.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
